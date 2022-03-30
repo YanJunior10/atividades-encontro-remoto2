89817f875e4e94216fa726940582ab1ba1cd6d3c
YanJunior10 <yan.souza@dentalspeed.com>
2022-03-30 13:27:28 -0300

    encontro remoto2 uc8

---
+++ script.js
@@ -0,0 +1,16 @@
+// criar um contador de 0 a 10
+
+for (let contador = 0;contador <= 10; contador++)
+{
+//código sera repetido
+console.log(contador)
+// localicar numero 8
+    if (contador == 8){
+    // resultado da condição for verdadeiro
+        console.log("o numero foi localizado")
+    }else{
+    // resultado da consição falso
+    console.log("o numero não foi localizado")
+    }
+
+}// criar um laço para exibir se o o numero é impar ou par
---
+++ script3.js
@@ -0,0 +1,11 @@
+// registrar 3 nomes
+// Array com 4 posições
+
+let nomes = ["Carlos","Ronaldo","Odirley","Cinthia"];
+
+
+
+for(let contador = 0; contador <= 3; contador ++)
+{
+    console.log(nomes[contador]);
+}
